f622551689d9913adfc56b1c0d2e3b99c288fe37
tania <12tani345@gmail.com>
2026-07-02 22:44:45 +0600

    chore: remove static export for netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+ 
   reactCompiler: true,
    images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
